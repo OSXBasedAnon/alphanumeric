e5eac441fa4c3d44ecc4905ac1ddf5148df8302f
Kume001 <119001570+Kume001@users.noreply.github.com>
2025-06-06 17:08:53 +0300

    Update build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,41 @@
+@@ -1,7 +1,36 @@
 use winres::WindowsResource;
+// alphanumeric builder supporting multiple target platforms: Windows, OSX, and Linux
+
+use std::env;
 
 fn main() {
     let mut res = WindowsResource::new();
     res.set_icon("app_icon.ico");
     res.compile().unwrap();
+    let target = env::var("TARGET").unwrap_or_default();
+    println!("cargo:rerun-if-changed=build.rs");
+    println!("cargo:rerun-if-changed=Cargo.toml");
+
+    if target.contains("windows") { // For Windows
+        println!("Building alphanumeric for Windows");
+
+        use winres::WindowsResource;
+        let mut res = WindowsResource::new();
+        res.set_icon("app_icon.ico");
+        res.compile().unwrap();
+    } else if target.contains("apple-darwin") { // For OSX
+        println!("Building alphanumeric for OSX");
+
+        let target_env = env::var("RUST_TARGET_ENV").unwrap_or_else(|_| "gnu".to_string());
+
+        if target_env != "gnu" && target_env != "msvc" {
+            panic!("Target environment must be 'gnu' or 'msvc'");
+        }
+    } else if target.contains("linux") { // For Linux
+        println!("Building alphanumeric for Linux");
+
+        let target_env = env::var("RUST_TARGET_ENV").unwrap_or_else(|_| "gnu".to_string());
+
+        if target_env != "gnu" && target_env != "musl" {
+            panic!("Unsupported target environment for Linux");
+        }
+    } else {
+        println!("Build script: Unsupported target platform.");
+    }
 }
